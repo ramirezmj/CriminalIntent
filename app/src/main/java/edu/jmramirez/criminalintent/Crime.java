343d737a7bbe46c49b2892985d23baf129acdd66
Jose Manuel Ramírez Martínez <savy@MacBook-Air.local>
2015-10-23 19:21:34 +0200

    Added fragments

---
+++ app/src/main/java/edu/jmramirez/criminalintent/Crime.java
@@ -0,0 +1,26 @@
+package edu.jmramirez.criminalintent;
+
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+}
